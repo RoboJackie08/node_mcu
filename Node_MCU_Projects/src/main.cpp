897c5b242ee40edf0dc00f8da014f8adde401bce
Robo Jackie <robojackie08@gmail.com>
2026-01-29 00:20:14 +0530

    Testing MQTT

--- Node_MCU_Projects/src/main.cpp
+++ Node_MCU_Projects/src/main.cpp
@@ -1,27 +1,98 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
-extern "C" {
-  #include <wifi_mgr.h>
-#include <gpio.h>
+/* ---------- WiFi ---------- */
+const char* ssid     = "Airtel_nand_2308";
+const char* password = "air51529";
+
+/* ---------- MQTT ---------- */
+const char* mqtt_server = "broker.hivemq.com";
+const int   mqtt_port   = 1883;
+
+/* ---------- MQTT Topics ---------- */
+const char* sub_topic = "test/nodemcu/led";
+const char* pub_topic = "test/nodemcu/status";
+
+/* ---------- Objects ---------- */
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+/* ---------- MQTT callback ---------- */
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+    String message;
+
+    for (unsigned int i = 0; i < length; i++) {
+        message += (char)payload[i];
+    }
+
+    Serial.print("Received [");
+    Serial.print(topic);
+    Serial.print("] ");
+    Serial.println(message);
+
+    if (String(topic) == sub_topic) {
+        if (message == "ON") {
+            digitalWrite(LED_BUILTIN, LOW);   // LED ON
+        } else if (message == "OFF") {
+            digitalWrite(LED_BUILTIN, HIGH);  // LED OFF
+        }
+    }
 }
 
-#define LED_PIN D4   // Built-in LED (active LOW)
+/* ---------- MQTT reconnect ---------- */
+void mqttReconnect() {
+    while (!client.connected()) {
+        Serial.print("Connecting to MQTT... ");
 
+        String clientId = "NodeMCU-";
+        clientId += String(ESP.getChipId());
 
+        if (client.connect(clientId.c_str())) {
+            Serial.println("clientId: " + clientId + " connected");
+            client.subscribe(sub_topic);
+        } else {
+            Serial.print("failed, rc=");
+            Serial.println(client.state());
+            delay(2000);
+        }
+    }
+}
 
-void setup()
-{
+void setup() {
     Serial.begin(115200);
-    delay(500);
 
-    intialise_gpio(LED_PIN, OUTPUT);
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, HIGH); // LED OFF
 
-    Serial.println("\n--- ESP8266 WiFi Demo ---");
+    /* WiFi connect */
+    WiFi.begin(ssid, password);
+    Serial.print("Connecting WiFi");
 
-    wifi_init();
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+
+    Serial.println("\nWiFi connected");
+    Serial.print("IP: ");
+    Serial.println(WiFi.localIP());
+
+    /* MQTT setup */
+    client.setServer(mqtt_server, mqtt_port);
+    client.setCallback(mqttCallback);
 }
 
-void loop()
-{
-    wifi_run();
+void loop() {
+    if (!client.connected()) {
+        mqttReconnect();
+    }
+
+    client.loop();
+
+    static unsigned long lastPublish = 0;
+    if (millis() - lastPublish > 5000) {
+        lastPublish = millis();
+        client.publish(pub_topic, "NodeMCU is alive");
+    }
 }
---
+++ Node_MCU_Projects/src/main_1.cpp
@@ -0,0 +1,27 @@
+// #include <Arduino.h>
+
+// extern "C" {
+//   #include <wifi_mgr.h>
+// #include <gpio.h>
+// }
+
+// #define LED_PIN D4   // Built-in LED (active LOW)
+
+
+
+// void setup()
+// {
+//     Serial.begin(115200);
+//     delay(500);
+
+//     intialise_gpio(LED_PIN, OUTPUT);
+
+//     Serial.println("\n--- ESP8266 WiFi Demo ---");
+
+//     wifi_init();
+// }
+
+// void loop()
+// {
+//     wifi_run();
+// }
